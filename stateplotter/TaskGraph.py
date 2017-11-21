6d96c9004de0cb49d110bfa07e5b710b57dfed79
martinpettersson <laxenmeflaxen+git@gmail.com>
2017-11-21 17:31:12 +0100

    task graph modularised

--- stateplotter/TaskGraph.py
+++ stateplotter/TaskGraph.py
@@ -1,6 +1,36 @@
 import pyqtgraph as pg
 import numpy as np
 
+positions = np.array([
+    [10,0],
+    [0,0],
+    [0,-10],
+    [0,10],
+    ], dtype=float)
+
+adjacencies = np.array([
+    [0,1],
+    [0,2],
+    [0,3],
+    [1,0]
+    ])
+
+symbols = ['o','o','o','o']
+sizes = [3 for i in range(0, 4)]
+
+lines = np.array([
+    (255,0,0,255,1),
+    (255,0,255,255,2),
+    (255,0,255,255,3),
+    (255,255,0,255,2)
+    ], dtype=[('red',np.ubyte),('green',np.ubyte),('blue',np.ubyte),('alpha',np.ubyte),('width',float)])
+
+
+def makeTaskGraph():
+    graph = TaskGraph()
+    graph.setData(pos=positions, adj=adjacencies, symbol=symbols, size=sizes,  pxMode = False)
+    return graph
+
 class TaskGraph(pg.GraphItem):
     def __init__(self):
         self.dragPoint = None
@@ -8,15 +38,21 @@ class TaskGraph(pg.GraphItem):
         self.textItems = []
         pg.GraphItem.__init__(self)
         self.scatter.sigClicked.connect(self.clicked)
+        self.setTexts( ["Running","Suspended","Ready","Blocked"])
+
+    def setActiveState():
+        return
         
+    def setOffset(self, x, y):
+        self.pos = positions + [x,y]
+        self.setData(pos=self.pos)
+
     def setData(self, **kwds):
-        self.text = kwds.pop('text', [])
         self.data = kwds
         if 'pos' in self.data:
             npts = self.data['pos'].shape[0]
             self.data['data'] = np.empty(npts, dtype=[('index', int)])
             self.data['data']['index'] = np.arange(npts)
-        self.setTexts(self.text)
         self.updateGraph()
         
     def setTexts(self, text):
@@ -32,7 +68,6 @@ class TaskGraph(pg.GraphItem):
         pg.GraphItem.setData(self, **self.data)
         for i,item in enumerate(self.textItems):
             item.setPos(*self.data['pos'][i])
-        
         
     def mouseDragEvent(self, ev):
         if ev.button() != QtCore.Qt.LeftButton:
--- stateplotter/stateplotter.py
+++ stateplotter/stateplotter.py
@@ -4,22 +4,12 @@ import pyqtgraph as pg
 from pyqtgraph.Qt import QtCore, QtGui
 import numpy as np
 
-from TaskGraph import TaskGraph
+from TaskGraph import TaskGraph, makeTaskGraph
 from Grid import Grid
 app = QtGui.QApplication([])
 
 grid=Grid()
-# layout = grid.layout
-# Enable antialiasing for prettier plots
 pg.setConfigOptions(antialias=True)
-
-# window = pg.GraphicsWindow()
-# window.setWindowTitle('stateplotter')
-# view = window.addViewBox()
-# view.setAspectLocked()
-
-# graph = TaskGraph()
-# view.addItem(graph)
 
 tasks_vb = pg.ViewBox()
 grid.tasks_widget.addItem(tasks_vb)
@@ -29,49 +19,18 @@ tasks_vb.setMouseEnabled(False, False)
 semphs_vb = pg.ViewBox()
 grid.semphs_widget.addItem(semphs_vb)
 
-## Define positions of nodes
-pos = np.array([
-    [10,0],
-    [0,0],
-    [0,-10],
-    [0,10],
-    [10,30],
-    [0,30],
-    [0,20],
-    [0,40]
-    ], dtype=float)
-    
-## Define the set of connections in the graph
-adj = np.array([
-    [0,1],
-    [0,2],
-    [0,3],
-    [1,0]
-    ])
-graph = pg.GraphItem(pos=pos, adj=adj)
-graph2 = pg.GraphItem(pos=pos, adj=adj)
-## Define the symbol to use for each node (this is optional)
-symbols = ['o','o','o','o','o','o','o','o']
-sizes = [3 for i in range(0, 8)]
-## Define the line style for each connection (this is optional)
-lines = np.array([
-    (255,0,0,255,1),
-    (255,0,255,255,2),
-    (255,0,255,255,3),
-    (255,255,0,255,2)
-    ], dtype=[('red',np.ubyte),('green',np.ubyte),('blue',np.ubyte),('alpha',np.ubyte),('width',float)])
+graph = makeTaskGraph()
+graph.setOffset(0,0)
 
+graph2 = makeTaskGraph()
+graph2.setOffset(0,40)
 
-texts = ["Running","Suspended","Ready","Blocked"]
-## Update the graph
-#vb.setData(pos=pos, adj=adj, pen=lines, size=1, symbol=symbols, pxMode=False, text=texts)
+graph3 = makeTaskGraph()
+graph3.setOffset(30,40)
 
-
-graph = pg.GraphItem(pos=pos, adj=adj, symbol=symbols, size=sizes,text=texts, pxMode = False)
-graph2 = pg.GraphItem(pos=pos, adj=adj, text=texts)
+tasks_vb.addItem(graph3)
+tasks_vb.addItem(graph2)
 tasks_vb.addItem(graph)
-semphs_vb.addItem(graph2)
-
 
 ## Start Qt event loop unless running in interactive mode or using pyside.
 if __name__ == '__main__':
