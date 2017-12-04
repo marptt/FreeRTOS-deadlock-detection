5b779e564a2b685a40b26e530418ac93789587d7
martinpettersson <laxenmeflaxen+git@gmail.com>
2017-12-04 04:10:17 +0100

    changed from GraphItem logic to ScatterPlotItem

--- stateplotter/TaskGraph.py
+++ stateplotter/TaskGraph.py
@@ -1,6 +1,7 @@
 import pyqtgraph as pg
 import numpy as np
 
+
 positions = np.array([
     [10,0],
     [0,0],
@@ -19,27 +20,53 @@ symbols = ['o','o','o','o']
 sizes = [3 for i in range(0, 4)]
 
 lines = np.array([
-    (255,0,0,255,1),
+    (255,0,0,  255,1),
     (255,0,255,255,2),
     (255,0,255,255,3),
     (255,255,0,255,2)
     ], dtype=[('red',np.ubyte),('green',np.ubyte),('blue',np.ubyte),('alpha',np.ubyte),('width',float)])
 
+BLUE = (0, 0, 100, 255)
+RED = (100,0,0, 255)
 
-class TaskGraph(pg.GraphItem):
-    def __init__(self):
-        self.tasks = []
+brush=pg.mkBrush(color=(200, 10, 0,1))
+
+brush = (30, 100, 2, 255)
+
+class TaskGraph(pg.GraphItem):    
+    def __init__(self, x_offset, y_offset):
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+        
+        self.setCurrentState('Ready')
         self.dragPoint = None
         self.dragOffset = None
         self.textItems = []
         pg.GraphItem.__init__(self)
-        self.setData(pos=positions, adj=adjacencies, symbol=symbols, size=sizes,  pxMode = False)
-        self.scatter.sigClicked.connect(self.clicked)
+        # self.setData(pos=positions, adj=adjacencies, symbol=symbols, size=sizes,  pxMode = False,symbolBrush = brush)
+        # self.setData(adj=adjacencies, symbol=symbols, size=sizes,  pxMode = False,symbolBrush = brush)
+
+        self.scatter.addPoints([
+            {
+                'pos': (self.nodes[key]['x'] + x_offset, self.nodes[key]['y'] + y_offset),
+                'size': 100,
+                'brush': BLUE
+            } for key in self.nodes 
+        ])
+
         self.setTexts( ["Running","Suspended","Ready","Blocked"])
         
-    def setOffset(self, x, y):
-        self.pos = positions + [x,y]
-        self.setData(pos=self.pos)
+    def setCurrentState(self, state):
+        self.nodes = {}
+        self.nodes['Running'] =   {'x': 10,'y': 0,  'color': BLUE} 
+        self.nodes['Suspended'] = {'x': 0, 'y': 0,  'color': BLUE} 
+        self.nodes['Ready'] =     {'x': 0, 'y': -10,'color': BLUE}
+        self.nodes['Blocked'] =   {'x': 0, 'y': 10, 'color': BLUE}
+        
+        self.nodes[state]['color'] = RED
+       
+        for key in self.nodes:
+            node = self.nodes[key]            
 
     def setData(self, **kwds):
         self.data = kwds
@@ -62,18 +89,23 @@ class TaskGraph(pg.GraphItem):
         pg.GraphItem.setData(self, **self.data)
         for i,item in enumerate(self.textItems):
             item.setPos(*self.data['pos'][i])
-        
-    def clicked(self, pts):
-        print("clicked: %s" % pts)
-        self.brush = pg.mkColor(100,255, 100)
-
-    def blobItem(self, x, y, color, text):
-        poop
-        
-
-class TaskGraphWidget():
+         
+class TaskGraphWidget(pg.GraphicsView):
     def __init__(self, stateHandler):
-        self.graph = TaskGraph()
+        
+        #tasks_vb.addItem(pg.ArrowItem(angle=-120, tipAngle=30, baseAngle=20, headLen=40, tailLen=40, tailWidth=8, pen=None, brush='y'))
+
+        graph = TaskGraph(0,0)
+        #taskGraph = taskWidget.graph
+        tasks_vb = pg.ViewBox()
+        tasks_vb.addItem(graph)
+        tasks_vb.setAspectLocked(1.0)
+        tasks_vb.setMouseEnabled(False, False)
+        
+        pg.GraphicsView.__init__(self)
+        self.addItem(tasks_vb)
+        self.setCentralWidget(tasks_vb)
+
         self.stateHandler = stateHandler
         self.stateHandler.subscribeToCurrentState(self.onStateChange)
         
@@ -83,3 +115,6 @@ class TaskGraphWidget():
 
             
         
+
+
+
--- stateplotter/stateplotter.py
+++ stateplotter/stateplotter.py
@@ -13,28 +13,14 @@ layout = QtGui.QHBoxLayout()
 stateHandler = StateHandler()
 
 taskWidget = TaskGraphWidget(stateHandler)
-taskGraph = taskWidget.graph
-taskGraph.setOffset(0,0)
-tasks_vb = pg.ViewBox()
-tasks_vb.addItem(taskGraph)
-tasks_vb.setAspectLocked(1.0)
-tasks_vb.setMouseEnabled(False, False)
-
-
-tasks_v = pg.GraphicsView()
-tasks_v.addItem(tasks_vb)
-tasks_v.setCentralWidget(tasks_vb)
-
 
 tasks_vb2 = pg.ViewBox()
 tasks_v2 = pg.GraphicsView()
 tasks_v2.setCentralWidget(tasks_vb2)
 
-
 listwidget = EventLog(stateHandler)
 
-
-layout.addWidget(tasks_v)
+layout.addWidget(taskWidget)
 layout.addWidget(tasks_v2) 
 layout.addWidget(listwidget)   
 
@@ -44,11 +30,7 @@ layout.setStretch(2,1)
 
 w.setLayout(layout)
 
-# connect up subscribers
-# TODO MOVE
-# stateHandler.subscribeToCurrentState(taskGraph.onStateChange)
-# stateHandler.subscribeToCurrentState(placeholderGraph.onStateChange)
-# stateHandler.subscribeToState(eventLog.onStateChange) maybe not needed
+
 stateHandler.testStates()
 
 
